b44938a9cedba3cf48b714d67f31c99bb92c91f9
dalvijayshri <dalvi.jayshri24@gmail.com>
2016-08-16 19:58:38 -0400

    Login Form Design  Created

---
+++ ClinicSystem/ClinicSystem/frmLogin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClinicSystem
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlMiddle_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Timer_SystemDateTime_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlTop_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
